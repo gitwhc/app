9763d3ef8685df51e7b3e66805fc4ea9aa3afe68
gitwhc <2208440160@qq.com>
2023-12-13 20:16:26 +0800

    a

--- client/main.cpp
+++ client/main.cpp
@@ -45,7 +45,7 @@ int main(){
         while(1){
             cin>>cho_1;
             if(cho_1==1){
-                buf[0]='1';
+                buf[0]='122';
                 send(stream,buf,1,0);
             }
             if(cho_1==2){
